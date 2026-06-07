79fd6b3f166fe8557ae71f2a3197c138e6fe4877
Arpit Verma <arpitv970@gmail.com>
2026-06-07 18:20:53 +0530

    done w/ armstrong num

--- 04-basic-maths/01-digit-concept.cpp
+++ 04-basic-maths/01-digit-concept.cpp
@@ -13,12 +13,52 @@ int revNum(int n) {
   return temp;
 }
 
+int reverse(int x) {
+  int rev = 0;
+  while (x != 0) {
+    int last = x % 10;
+    if (rev < INT_MIN / 10 || rev > INT_MAX / 10) {
+      return 0;
+    }
+
+    rev = rev * 10 + last;
+    x /= 10;
+  }
+
+  return rev;
+}
+
+int armstrongNum(int x) {
+  int ans = 0;
+
+  while (x != 0) {
+    int last = x % 10;
+
+    if (ans < INT_MIN / 10 || ans > INT_MAX / 10) {
+      return 0;
+    }
+
+    ans = ans + (last * last * last);
+    x /= 10;
+  }
+
+  return ans;
+}
+
 int main() {
   int n;
   cout << "Enter any large digit number (under int length): ";
   cin >> n;
 
-  int ans = revNum(n);
-
+  int ans = reverse(n);
   cout << ans << endl;
+
+  int armNum = armstrongNum(n);
+
+  cout << "arm: " << armNum << endl;
+  if (armNum == n) {
+    cout << "this is the guy!!" << endl;
+  } else {
+    cout << "na i'd win" << endl;
+  }
 }
